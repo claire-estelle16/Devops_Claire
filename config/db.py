42171fd02977380cdea79d25e0663abf0243f932
claire-estelle16 <zoboclaireestelle@gmail.com>
2025-05-09 18:35:13 +0200

    config db.py and .env

--- config/db.py
+++ config/db.py
@@ -1,5 +1,14 @@
+import os
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/db_devops")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "db_devops")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 meta = MetaData()
 con = engine.connect()
